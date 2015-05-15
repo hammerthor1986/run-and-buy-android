a81a4077a08474fcb67b02274654abb2f4c1929e
AMS2-20 <palladin.93@hotmail.com>
2015-05-15 20:05:15 +0200

    Cambios varios

--- app/src/main/java/com/example/ams2_21/registrom13/LoginActivity.java
+++ app/src/main/java/com/example/ams2_21/registrom13/LoginActivity.java
@@ -1,51 +1,24 @@
 package com.example.ams2_21.registrom13;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-
 import android.app.Activity;
-import android.app.LoaderManager.LoaderCallbacks;
 import android.app.ProgressDialog;
-import android.content.ContentResolver;
-import android.content.CursorLoader;
 import android.content.Intent;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
-import org.apache.http.message.BasicNameValuePair;
-
 public class LoginActivity extends Activity implements OnClickListener {
+    private static Firebase ref = new Firebase("https://run-n-buy.firebaseio.com");
 
     private EditText user, pass;
     private Button mSubmit, mRegister;
-    private ProgressDialog pDialog;
-    private static Firebase ref = new Firebase("https://run-n-buy.firebaseio.com");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
